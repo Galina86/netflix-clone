99ee5b95cd80ac4292965c7243dc00d97e267fcc
Galina Ostrovskaia <galya.ostrovskaya@gmail.com>
2023-06-22 16:40:56 -0400

    add routes

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+import MainPage from "../components/MainPage";
+import MoviePage from "./MoviePage";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="movie">
+        <Route path=":id" element={<MoviePage />}></Route>
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRouter;
--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "../axios";
-import "./Row.css"
+import "./Row.css";
+import { useNavigate } from "react-router-dom";
 
 const img_base_URL = "https://image.tmdb.org/t/p/original";
 
-const Row = ({ title, fetchURL, isLargeRow }: { title: string; fetchURL: string; isLargeRow?: boolean}) => {
+const Row = ({
+  title,
+  fetchURL,
+  isLargeRow,
+}: {
+  title: string;
+  fetchURL: string;
+  isLargeRow?: boolean;
+}) => {
   const [movies, setMovies] = useState<any[]>([]);
 
   useEffect(() => {
@@ -16,16 +25,25 @@ const Row = ({ title, fetchURL, isLargeRow }: { title: string; fetchURL: string;
     fetchData();
   }, [fetchURL]);
 
+  let navigate = useNavigate();
+  const handleClick = (movie: { id: number }) => {
+    let path = `/movie/${movie.id}`;
+    navigate(path);
+  };
+
   return (
-    <div className ="row">
+    <div className="row">
       <h2>{title}</h2>
 
       <div className="row_posters">
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
-            src={`${img_base_URL}${isLargeRow  ? movie.poster_path : movie.backdrop_path}`}
+            src={`${img_base_URL}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.title}
           />
         ))}
