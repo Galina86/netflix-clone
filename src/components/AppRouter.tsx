a31ac5736b7acf0174a1ae127142bdbd56a4b22e
karinayatchenko <karina.yatchenko@hellofresh.com>
2023-07-15 16:25:02 -0400

    add redux

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -4,18 +4,23 @@ import MoviePage from "./MoviePage";
 import LoginScreen from "./screens/loginScreen/LoginScreen";
 import { useEffect } from "react";
 import { auth } from "../firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "../features/userSlice";
 
 const AppRouter = () => {
 
-  const user = null;
-  
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch()
 
   useEffect(()=>{
     const unsubscribe = auth.onAuthStateChanged(userAuth =>{
       if(userAuth){
-         console.log("@@@", userAuth)
+         dispatch(login({
+          uid: userAuth.uid,
+          email : userAuth.email,
+         }))
       } else{
-
+        dispatch(logout)
       }
     });
 
