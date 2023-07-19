fd65b36137adcfcb0ab255eb928189ba7f6afe1f
karinayatchenko <karina.yatchenko@hellofresh.com>
2023-07-19 19:16:10 -0400

    add functionality to play button

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,6 +2,11 @@ import axios from "../axios";
 import { useEffect, useState } from "react";
 import requests from "../requests";
 import "./Banner.css";
+import movieTrailer from "movie-trailer";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import YouTube from "react-youtube";
+import closeIcon from '../assets/images/close_white.png'
 
 type Movie = {
   backdrop_path: string;
@@ -14,6 +19,12 @@ type Movie = {
 
 function Banner() {
   const [movie, setMovie] = useState<Movie>();
+  const [result, setResult] = useState<any>();
+  const [trailerURL, setTrailerURL] = useState<string | null>(null);
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   useEffect(() => {
     async function fetchData() {
@@ -32,6 +43,27 @@ function Banner() {
     return str.length > max ? str.substr(0, max - 1) + "…" : str;
   }
 
+  // const handleClick = (result: any) => {
+  //   if (trailerURL) {
+  //     setTrailerURL("");
+  //   } else {
+  //     movieTrailer(result?.title || "")
+  //       .then((url: string) => {
+  //         const urlParams = new URLSearchParams(new URL(url).search);
+  //         setTrailerURL(urlParams.get("v"));
+  //       })
+  //       .catch((error: any) => console.log(error));
+  //   }
+  // };
+
+  // const options = {
+  //   height: "390",
+  //   width: "100%",
+  //   playerVars: {
+  //     autoplay: 1,
+  //   },
+  // };
+
   return (
     <header
       className="banner"
@@ -46,8 +78,24 @@ function Banner() {
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
         <div className="banner__buttons">
-          <button className="banner__button">Play</button>
+          <button className="banner__button" onClick={() => {
+                handleOpen();
+                // handleClick(result);
+              }} >Play</button>
           <button className="banner__button">My List</button>
+          <Modal
+              open={open}
+              onClose={handleClose}
+            >
+              <Box className="banner__trailer-window" >
+              <img src={closeIcon} alt="here image" onClick={handleClose} className="banner__trailer-close-icon"/> 
+                {/* {trailerURL ? (
+                  <YouTube videoId={trailerURL} opts={options} />
+                ) : ( */}
+                  <p className="banner__trailer-error">We are sorry, there is no trailer for this movie.</p>
+                {/* )} */}
+              </Box>
+            </Modal>
         </div>
         <h1 className="banner__description">
           {truncate(`${movie?.overview}`, 150)}
