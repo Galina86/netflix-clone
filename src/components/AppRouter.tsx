c1bbca7e3d403b1ad0cba2f983a4b5321a2b2367
Galina Ostrovskaia <galya.ostrovskaya@gmail.com>
2023-07-31 13:46:19 -0400

    fix flicker

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -2,19 +2,20 @@ import { Route, Routes } from "react-router";
 import MainPage from "./screens/mainPageScreen/MainPage";
 import MoviePage from "./screens/moviePageScreen/MoviePage";
 import LoginScreen from "./screens/loginScreen/LoginScreen";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { auth } from "../firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { login, logout, selectUser } from "../redux/userSlice";
 import ProfileScreen from "./screens/profileScreen/ProfileScreen";
-import { HOME_PAGE, PROFILE_PAGE, MOVIE_PAGE} from '../constants';
+import { HOME_PAGE, PROFILE_PAGE, MOVIE_PAGE } from "../constants";
 import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
 
 const AppRouter = () => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
-  const userInLocalStorage = localStorage.getItem("email");
+
+  const [isUserFetched, setIsUserFetched] = useState<boolean>(false);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
@@ -26,32 +27,35 @@ const AppRouter = () => {
             email: userAuth.email,
           })
         );
+        setIsUserFetched(true);
       } else {
         //Logged out
         dispatch(logout());
+        setIsUserFetched(false);
       }
     });
 
     return unsubscribe;
   }, [dispatch]);
 
-  // if (userInLocalStorage === "") {
-  //   return (
-  //     <Box
-  //       sx={{
-  //         display: "flex",
-  //         justifyContent: "center",
-  //         alignItems: "center",
-  //         backgroundColor: "black",
-  //         height: "100vh",
-  //       }}
-  //     >
-  //       <CircularProgress />
-  //     </Box>
-  //   );
-  // }
+  console.log("**** useer", user);
+  console.log("**** isUserFetched", isUserFetched);
 
-  console.log("user", !!userInLocalStorage);
+  if (!user && isUserFetched) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "black",
+          height: "100vh",
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
 
   return (
     <Routes>
